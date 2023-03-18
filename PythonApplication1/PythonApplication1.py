0b47fdbbe90156abdb22f750c78ddc9b058de252
Vasin Igor <vasin.igor0796@gmail.net>
2023-03-18 13:35:50 +0200

    Ex_2 Task done

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,1 +1,6 @@
 
+salary = int(input('Введіть запрлату за місяць:'))
+payment = int(input('Введіть місячний платіж за кредитом:'))
+debt = int(input('Введіть суму заборгованості за комунальні послуги:'))
+result = salary-payment-debt
+print('Сума залишку грошей:', result)
